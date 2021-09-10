2377d818830e87df72be4215ee5ee070d050e79c
Tuzkiss <Tuzkiss1017@gmail.com>
2021-09-10 17:03:09 +0800

    Update

--- codecs-video.js
+++ codecs-video.js
@@ -1,6 +1,21 @@
 var VIDEO_CODECS = [
 	{ codec: "hev1.1.6.L93.B0", description: "HEVC progressive, non-packed stream, Main Profile, Main Tier, Level 3.1"},
 
+	{ codec: "hev1.1.6.L120.90", description: "HEVC progressive, non-packed stream, Main Profile, Main Tier, Level 4 ( 1080P )"},
+
+	{ codec: "hev1.1.6.L153.90", description: "HEVC progressive, non-packed stream, Main Profile, Main Tier, Level 5.1 ( 2160P )"},
+
+	{ codec: "hev1.1.6.L156.90", description: "HEVC progressive, non-packed stream, Main Profile, Main Tier, Level 5.2"},
+
+	{ codec: "hev1.2.4.L150.90", description: "HEVC progressive, non-packed stream, Main 10 Profile, Main Tier, Level 5.0"},
+
+	{ codec: "hev1.2.4.L156.90", description: "HEVC progressive, non-packed stream, Main 10 Profile, Main Tier, Level 5.2"},
+
+	{ codec: "dvh1.05.06", description: "Dolby Vision 2160P"},
+
+	{ codec: "ac-3", description: "Dolby Digital"},
+	{ codec: "ec-3", description: "Dolby Digital Plus"},
+
 	// vp09.<profile>.<level>.<bitDepth>[.<chromaSubsampling>.<colourPrimaries>.<transferCharacteristics>.<matrixCoefficients>.<videoFullRangeFlag>]
 	{ codec: "vp09.00.50.08", description: "VP9, Profile 0, Level 5, 8-bit"},
 ];
